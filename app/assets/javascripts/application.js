f11fec4c673c234ed333d8ea46b3a1561810728f
Sam Assadi <samsassadi@gmail.com>
2016-04-23 13:53:37 -0700

    graph section - incomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,25 @@
-//= require jquery
-//= require bootstrap
-//= require jquery_ujs
-//= require turbolinks
+$(function () {
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
 
-//= require_tree .
+        // Create the chart
+        $('#graph').highcharts('StockChart', {
 
+            rangeSelector: {
+                selected: 1
+            },
+
+            title: {
+                text: 'AAPL Stock Price'
+            },
+
+            series: [{
+                name: 'AAPL Stock Price',
+                data: data,
+                type: 'spline',
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+    });
+});
