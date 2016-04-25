c4663c6fc2b77573b403d4d6f7a769ba3a65b81c
wly731 <wly731@gmail.com>
2016-04-25 00:49:55 -0700

    add .env and add api keys as environment variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -84,7 +84,7 @@ class ApplicationController < ActionController::Base
     # result = RestClient.get 'http://api.nytimes.com/svc/search/v2/articlesearch.json?callback=svc_search_v2_articlesearch&q=finance&begin_date=20140101&end_date=20150101&sort=oldest&api-key=4135b3b218606cfe437e454cea3fca0f%3A0%3A75109961'
 
     link = URI.escape("http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+query+"&fq=news_desk:(\"Finance\",\"Business\",\"SundayBusiness\")&begin_date="+begin_date+"&end_date="+end_date+"&api-key=")
-    link += "4135b3b218606cfe437e454cea3fca0f%3A0%3A75109961"
+    link += ENV["NYTIMESKEY"]
     result = RestClient.get link
     return result
 
